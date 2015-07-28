e03ab07bd5c87818c6ba0def6cc0f4846d6cd160
Yuan Ganping <562720180@qq.com>
2015-07-28 16:47:58 +0800

    添加闭包

--- function.js
+++ function.js
@@ -44,3 +44,36 @@ var statusObject = {
 
 var status = Quo.prototype.get_status.apply(statusObject);
 console.log(status);
+
+
+//闭包
+var myObject = (function(){
+	var value = 0;
+
+	return {
+		increment: function(inc){
+			value += typeof inc === 'number' ? inc : 1;
+		},
+		getValue: function(){
+			return value;
+		}
+	};
+}());
+
+myObject.increment(3);
+console.log(myObject.getValue());
+
+
+//创建一个名为quo的构造函数
+//它构造出带有get_status方法和status属性的一个对象
+
+var quo = function(status){
+	return {
+		get_status: function(){
+			return status;
+		}
+	};
+};
+var myQuo = quo("amazed");
+
+console.log(myQuo.get_status());
